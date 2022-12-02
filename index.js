c49d62fd8ee8f040f34734db701529aa6f2a3382
Baptiste Jamin <baptiste@crisp.chat>
2022-12-02 16:58:55 +0100

    Bump

--- index.js
+++ index.js
@@ -50,10 +50,12 @@ var add_og_image_to_html = (file, buffer, options) => {
    // Remove Emojis as those are unsupported
   if (_title) {
     _title = _title.replace(EMOJI_REGEX, "");
+    _title.trim();
   }
 
   if (_description) {
     _description = _description.replace(EMOJI_REGEX, "");
+    _description.trim();
   }
 
   return Promise.resolve()
