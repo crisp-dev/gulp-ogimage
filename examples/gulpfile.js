e3ba13ef59f546950f4032bd3948a039ac2365a6
Baptiste Jamin <baptiste@crisp.chat>
2022-12-02 16:49:52 +0100

    nz

--- examples/gulpfile.js
+++ examples/gulpfile.js
@@ -4,12 +4,11 @@ var ogImage = require('../');
 gulp.task("default", function(){
   return gulp.src('./index.html')
   .pipe(ogImage({
+    base : function() {
+      return "https://example.com/"
+    },
     directory : function() {
       return "./build/images"
-    },
-
-    title : function($) {
-      return "title"
     }
   }))
   .pipe(gulp.dest("./build"));
